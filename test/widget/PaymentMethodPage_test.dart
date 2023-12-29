2f79778990e2210b74272b653b11c6e4ec542f42
EurekaCHWARD <eurekadiaandisy8@gmail.com>
2023-12-29 13:03:50 +0700

    lol

--- test/widget/PaymentMethodPage_test.dart
+++ test/widget/PaymentMethodPage_test.dart
@@ -6,7 +6,6 @@ import 'package:booking_hotel/Views/PaymentMethodPage.dart';
 
 void main() {
   testWidgets('PaymentMethodPage UI Test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
     final PaymentModel paymentModel = PaymentModel(
       pricePerNight: 100.0,
       tax: 10.0,
@@ -20,15 +19,12 @@ void main() {
       ),
     );
 
-    // Verify the UI elements.
     expect(find.text('Metode Pembayaran'), findsOneWidget);
     expect(find.text('Pilih Metode Pembayaran'), findsOneWidget);
 
-    // Simulate user interaction, e.g., tap on a button.
     await tester.tap(find.text('Confirm'));
     await tester.pump();
 
-    // You can add more test cases as needed.
 
   });
 }
